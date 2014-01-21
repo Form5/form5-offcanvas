6b17f11a195745e0adb5c772359a655e7116f4cd
Benedikt D Valdez <benediktvaldez@gmail.com>
2014-01-21 11:53:07 +0000

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,165 @@
 form5-offcanvas
 ===============
+
+form5-offcanvas is a powerful jQuery plugin that enables you to use an
+offcanvas sidebar on your site. Only **4KB** of minified
+Javascript, along with your own styles (very basic demo styles are provided).
+
+[Demo](http://form5.github.io/form5-offcanvas/)
+
+## Getting started
+
+### How to fetch form5-offcanvas?
+
+Install with [Bower](http://bower.io): `bower install form5-offcanvas`
+
+Clone the Github project: `git clone https://github.com/Form5/form5-offcanvas.git`
+
+Or [download zip](https://github.com/Form5/form5-offcanvas/archive/master.zip).
+
+### Then what?
+
+After installation simply load the files along with other styles and scripts.
+Remember that this script requires jQuery, meaning jQuery should already be
+loaded before this script. We recommend using the latest stable release.
+
+```html
+<link rel="stylesheet" href="path/to/form5-offcanvas.css">
+...
+<script src="path/to/jquery.js"></script>
+<script src="path/to/form5-offcanvas.min.js"></script>
+<script>
+  $(function(){
+    $('offcanvas').offcanvas();
+  });
+</script>
+```
+
+Construct your content as demonstrated below. In case of disabled javascript,
+styles should take care of downgrading your sidebar to either visible content
+or what ever fits your needs. Note that the `<offcanvas>` element can be
+literally anything, for example `nav` or just an old school `div`. Just make
+sure to use the appropriate selector when enabling the script (check out our
+[demo](http://form5.github.io/form5-offcanvas/)).
+
+```html
+<body>
+  <main>
+    ...
+  </main>
+  <offcanvas>
+    ...
+  </offcanvas>
+</body>
+```
+
+## Advanced usage
+
+There are a few ways to configure the scripts functionality. Here you can see
+all the options with their default values, and below you can find more
+detailed description for each option.
+
+```javascript
+$('offcanvas').offcanvas({
+  toggle: '.toggle-canvas',
+  canvas: ['main'],
+  open: function(){},
+  close: function(){},
+  init: function(){},
+  debug: false
+});
+```
+
+`toggle: string (selector)` **Default: '.toggle-canvas'**
+
+Selector for elements that should toggle the sidebar, usually some kind of
+button, obviously depending on your markup.
+
+`canvas: array (of selectors)`**Default: ['main']**
+
+An array of elements that should be considered a part of the 'canvas', meaning
+elements that should be animated to the side on toggle. Usually this should
+only be the main wrapping element of your page, but sometimes we need to do
+things like fixed navigation requiring an element to be outside that should
+still be treated as if it were inside (this is getting way to complicated).
+Basically, all elements that should be moved out of the way from our offcanvas
+sidebar. **Note:** the main wrapping element should be listed first, and the
+others should follow, if any.
+
+`open: function ()` **Default: function() {}**
+
+Optional function to be ran each time the sidebar is opened.
+
+`close: function ()` **Default: function() {}**
+
+Optional function to be ran each time the sidebar is closed.
+
+`init: function ()` **Default: function() {}**
+
+Optional function to be ran at initialization.
+
+`debug: boolean` **Default: false**
+
+If you are having problems and are not sure certain parts of the scripts are
+actually working, you can enable this option to receive console.logs with
+messages for initialization, opening, closing and destroying.
+
+### After initialization
+
+For js vs no-js styling, an `[alive='true']` attribute is included after
+initialization. See demo styles for an idea of usage.
+
+To toggle the offcanvas sidebar outside of the default triggers, you can
+simply call the function again with no options, as demonstrated below. In a
+similar manner we can explicitly open or close our sidebar by using these
+helper strings.
+
+```javascript
+// Toggle sidebar
+$('offcanvas').offcanvas();
+// Open sidebar
+$('offcanvas').offcanvas('open');
+// Close sidebar
+$('offcanvas').offcanvas('close');
+```
+
+Sometimes we do things we wish we hadn't, and just wanna take a step back and
+rethink the situation. For those instances, we included a helper string that
+enables you to simply reverse all changes made by this script.
+
+```javascript
+$('offcanvas').offcanvas('destroy');
+```
+
+After running this, you can initialize a fresh offcanvas with new options
+or whatever your mind can imagine.
+
+### Still not satisfied?
+
+You could go all in and customize the script directly. The original can be
+found in `src/form5-offcanvas.js`. To compile a minified/uglified version of
+your new script you can build it again by running:
+
+```shell
+$ npm install
+$ grunt build
+```
+
+If you would like to extend the core functionality of this script in a way
+that could benefit others, don't hesitate to create a pull request!
+
+## Cress
+
+### Using [Cress](http://github.com/Form5/Cress)
+
+If you are using our awesome CSS framework Cress, you can find a `.scss` file
+using the appropriate variables in `src/form5-offcanvas.cress.scss`. That way
+you have a basic offcanvas sidebar working directly with your current styles.
+
+## Author
+Written by [Benedikt D Valdez](http://github.com/benediktvaldez), developer at
+[Form5](http://www.form5.is).
+
+### Contributors
+[Olafur Nielsen](http://twitter.com/olafurnielsen), co-founder at
+[Form5](http://www.form5.is).
---
+++ src/form5-offcanvas.js
@@ -0,0 +1,244 @@
+
+(function() {
+  'use strict';
+  var pluginName = 'offcanvas',
+    defaults = {
+      toggle: '.toggle-canvas',
+      canvas: ['main'],
+      open: function(){},
+      close: function(){},
+      init: function(){},
+      debug: false
+    };
+
+
+  $[pluginName] = function (element, options) {
+    this.element = element;
+    this.$offcanvas = $(element);
+
+    this.options = $.extend({}, defaults, options);
+    this.debug = this.options.debug;
+
+    this._defaults = defaults;
+    this._name = pluginName;
+
+    var initCB = $.Callbacks('stopOnFalse');
+    initCB.add($.proxy(this.initialize,this));
+    if (typeof this.options.init === 'function') {
+      initCB.add($.proxy(this.options.init,this));
+    }
+    initCB.fire();
+  };
+
+  $[pluginName].prototype = {
+    initialize: function() {
+      if (this.debug) console.log('init offcanvas', this);
+
+      this.$offcanvas.attr('alive',true);
+      this.processSelectors();
+      this.attachEvents();
+      this.setSizes();
+    },
+
+    toggle: function(e) {
+      if (e) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+      if (this.state.open) {
+        return this.close();
+      } else {
+        return this.open();
+      }
+    },
+
+    open: function() {
+      if (this.debug) console.log('open offcanvas', this);
+      var openCB = $.Callbacks('stopOnFalse');
+
+      openCB.add($.proxy(function() {
+        this.state.open = true;
+
+        this.setBodyPosition(this.$offcanvas.data('width'));
+        this.state.position = this.$offcanvas.data('width');
+
+        this.$offcanvas.css({'-webkit-overflow-scrolling': 'touch'});
+        this.$offcanvas.attr('active',true);
+
+        this.el.toggle.attr('active',true);
+        this.el.toggle.attr('moving',true);
+
+        setTimeout($.proxy(function() {
+          this.el.toggle.attr('moving',false);
+          this.el.toggle.attr('open',true);
+        }, this), 250);
+      },this));
+      if (typeof this.options.open === 'function') {
+        openCB.add($.proxy(this.options.open,this));
+      }
+      openCB.fire();
+    },
+
+    close: function() {
+      if (this.debug) console.log('close offcanvas', this);
+      var closeCB = $.Callbacks('stopOnFalse');
+
+      closeCB.add($.proxy(function() {
+        this.state.open = false;
+
+        this.setBodyPosition(0);
+        this.state.position = 0;
+
+        this.$offcanvas.css({'-webkit-overflow-scrolling': ''});
+        this.$offcanvas.attr('active',false);
+
+        this.el.toggle.attr('active',false);
+        this.el.toggle.attr('moving',true);
+
+        setTimeout($.proxy(function() {
+          this.el.toggle.attr('moving',false);
+          this.el.toggle.attr('open',false);
+        }, this), 250);
+      },this));
+      if (typeof this.options.close === 'function') {
+        closeCB.add($.proxy(this.options.close,this));
+      }
+      closeCB.fire();
+    },
+
+    setBodyPosition: function(num) {
+      $.each(this.el.canvas, $.proxy(function(index, el) {
+        $(el).css(this.css.translate(num));
+        if (!$('html').hasClass('noscroll') ? num > 0 : void 0) {
+          $('html,body').addClass('noscroll');
+        }
+        if ($('html').hasClass('noscroll') ? num === 0 : void 0) {
+          $('html,body').removeClass('noscroll');
+        }
+      }, this));
+
+    },
+
+    processSelectors: function() {
+      this.el = {};
+
+      this.el.toggle = this.options.toggle;
+      if (typeof this.el.toggle === 'string') {
+        this.el.toggle = $(this.el.toggle);
+      }
+
+      if (typeof this.options.canvas === 'object' && typeof this.options.canvas[0] === 'string') {
+        this.el.canvas = [];
+        $.each(this.options.canvas, $.proxy(function(index, el) {
+          this.el.canvas.push($(el));
+          $(el).attr('canvas',true);
+        }, this));
+      } else {
+        this.el.canvas = false;
+      }
+    },
+
+    attachEvents: function() {
+      $(window).on('toggleCanvas', $.proxy(this.toggle,this));
+
+      this.el.toggle.on('click', function() {
+        $(window).trigger('toggleCanvas');
+      });
+
+      this.resizeTimeout = 0;
+      $(window).on('offCanvasResize', $.proxy(this.onResize, this));
+      $(window).on('resize scroll', function() {
+        $(window).trigger('offCanvasResize');
+      }).trigger('resize');
+    },
+
+    onResize: function() {
+      clearTimeout(this.resizeTimeout);
+      this.resizeTimeout = setTimeout($.proxy(function() {
+        this.setSizes();
+        if (this.el.toggle.css('display') === 'none' && this.state.open) {
+          return this.close();
+        }
+      }, this, 100));
+    },
+
+    setSizes: function() {
+      if (window.innerHeight) {
+        this.$offcanvas.css('height',window.innerHeight);
+        this.el.canvas[0].css('min-height', window.innerHeight);
+      } else {
+        this.$offcanvas.css('height',$(window).height());
+        this.el.canvas[0].css('min-height', $(window).height());
+      }
+      if (this.$offcanvas.css('left') === '0px') {
+        this.$offcanvas.data('side', 'left');
+      }
+      if (this.$offcanvas.css('right') === '0px') {
+        this.$offcanvas.data('side', 'right');
+      }
+      this.$offcanvas.data('width', parseInt(this.$offcanvas.css('min-width'), 10));
+      if (this.$offcanvas.data('side') === 'right') {
+        this.$offcanvas.data('width', -this.$offcanvas.data('width'));
+      }
+      if (this.state.open) {
+        return this.setBodyPosition(this.$offcanvas.data('width'));
+      }
+    },
+
+    state: {
+      open: false,
+      position: 0
+    },
+
+    css: {
+      translate: function(value) {
+        return {
+          '-webkit-transform': 'translate3d(' + value + 'px,0,0)',
+          '-moz-transform': 'translate3d(' + value + 'px,0,0)',
+          '-ms-transform': 'translate3d(' + value + 'px,0,0)',
+          '-o-transform': 'translate3d(' + value + 'px,0,0)',
+          'transform': 'translate3d(' + value + 'px,0,0)'
+        };
+      }
+    },
+
+    destroy: function() {
+      if (this.debug) console.log('destroying offcanvas', this);
+
+      this.close();
+      $(window).off('toggleCanvas offCanvasResize');
+      if (this.el.canvas) {
+        $.each(this.el.canvas, $.proxy(function(index, el) {
+          $(el).attr('canvas',false);
+        }, this));
+      }
+      this.$offcanvas.css('height','');
+      this.el.canvas[0].css('min-height','');
+      this.$offcanvas.attr('alive',false);
+    }
+  };
+
+  return $.fn[pluginName] = function(options) {
+    return this.each(function() {
+      if (!$(this).data('form5-'+pluginName)) {
+        return $(this).data('form5-'+pluginName, new $[pluginName](this, options));
+      } else {
+        var $form5Offcanvas = $(this).data('form5-'+pluginName);
+        switch (options) {
+          case 'open':
+            $form5Offcanvas.open();
+            break;
+          case 'close':
+            $form5Offcanvas.close();
+            break;
+          case 'destroy':
+            $form5Offcanvas.destroy();
+            $(this).removeData('form5-'+pluginName);
+            break;
+          default:
+            $form5Offcanvas.toggle();
+        }
+      }
+    });
+  };
+}).call(this);
